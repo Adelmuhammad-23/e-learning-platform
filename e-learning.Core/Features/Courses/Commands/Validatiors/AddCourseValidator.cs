f7845840c37112188e1142f72a6b82bf1c36f489
Adel Muhammad <141270987+Adelmuhammad-23@users.noreply.github.com>
2025-04-13 18:10:54 +0200

    Feat: push AddVideoInCourse endpoint (#4)
    Co-authored-by: Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>

---
+++ e-learning.Core/Features/Courses/Commands/Validatiors/AddCourseValidator.cs
@@ -0,0 +1,47 @@
+﻿using e_learning.Core.Features.Courses.Commands.Models;
+using FluentValidation;
+
+namespace e_learning.Core.Features.Courses.Commands.Validatiors
+{
+    public class AddCourseValidator : AbstractValidator<AddCourseCommand>
+    {
+        #region Constructors
+
+        public AddCourseValidator()
+        {
+            ApplyValidationsRules();
+        }
+        #endregion
+
+        #region Handel Functions
+
+        public void ApplyValidationsRules()
+        {
+            RuleFor(x => x.Price)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(x => x.UpdatedAt)
+               .NotEmpty()
+                .NotNull();
+            RuleFor(x => x.InstructorId)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(x => x.InstructorId)
+               .NotEmpty()
+                .NotNull();
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(x => x.CategoryId)
+               .NotEmpty()
+                .NotNull();
+            RuleFor(x => x.Hours)
+                .NotEmpty()
+                .NotNull();
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .NotNull();
+        }
+        #endregion
+    }
+}
