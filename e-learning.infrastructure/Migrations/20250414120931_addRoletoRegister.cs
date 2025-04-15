3befe93cfce0ebcbcdb47dc7b7aaf2412a10417a
Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>
2025-04-15 17:17:04 +0200

    - Implemented RoleSeeder and UserSeeder for initial data seeding. - Added DefaultRoles enum to manage predefined roles. - Updated User and Role entities. - Created migrations for role integration into RegisterCommand. - Updated authentication commands and validators accordingly.

---
+++ e-learning.infrastructure/Migrations/20250414120931_addRoletoRegister.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_learning.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRoletoRegister : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoleName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleName",
+                table: "AspNetUsers");
+        }
+    }
+}
