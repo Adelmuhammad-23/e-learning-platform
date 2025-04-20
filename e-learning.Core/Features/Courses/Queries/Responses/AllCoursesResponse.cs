9ab32e72250bdb702a38a5c01c29631886b92e93
Adel Muhammad <141270987+Adelmuhammad-23@users.noreply.github.com>
2025-04-20 13:58:02 +0200

    fix : update response for get all courses endpoint(#13)
    * Feat: push AddVideoInCourse endpoint
    
    * Feat(Modules): Add  addModule endpoint
    Fix(auth): Change ConfirmResetPassword end point from get to post
    
    * Feat(Videos):add video in module ,add Module management and change confirm reset password attribute
    
    * Fix : solve conflicts
    
    * Fix: Solve conflicts in ModuleMapping
    
    * test
    
    * fix : update response for get all courses endpoint
    
    ---------
    
    Co-authored-by: Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>

--- e-learning.Core/Features/Courses/Queries/Responses/AllCoursesResponse.cs
+++ e-learning.Core/Features/Courses/Queries/Responses/AllCoursesResponse.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string? Image { get; set; }
+
         public string Description { get; set; }
         public int InstructorId { get; set; }
         public decimal Price { get; set; }
