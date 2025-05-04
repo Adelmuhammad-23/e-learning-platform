882f57d2305566145f1c93d9001de0e9047b0d71
Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>
2025-05-04 22:28:28 +0300

    push

---
+++ e-learning.Services/Implementations/StudentServices.cs
@@ -0,0 +1,32 @@
+﻿using e_learning.Data.Entities;
+using e_learning.infrastructure.Repositories;
+using e_learning.Services.Abstructs;
+
+namespace e_learning.Services.Implementations
+{
+    public class StudentServices : IStudentServices
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentServices(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<string> AddStudentAsync(Student student)
+        {
+            var stu = await _studentRepository.AddStudentAsync(student);
+            if (stu != null)
+                return stu;
+            return "Can't add student because is null !";
+        }
+
+        public async Task<Student> GetStudentAsync(int id)
+        {
+            var stu = await _studentRepository.GetStudentAsync(id);
+            if (stu != null)
+                return stu;
+            return null;
+        }
+    }
+}
--- e-learning.Services/ModuleServicesDependencies.cs
+++ e-learning.Services/ModuleServicesDependencies.cs
@@ -18,6 +18,8 @@ namespace e_learning.Services
             services.AddTransient<IInstructorService, InstructorService>();
             services.AddTransient<IReviewService, ReviewService>();
             services.AddTransient<ICartService, CartService>();
+            services.AddTransient<IStudentServices, StudentServices>();
+
 
 
             return services;
