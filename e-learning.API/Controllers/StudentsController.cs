9b7ccdb3dd78b074a29e3cb1534b5ea687ecedb0
Adel Muhammad <141270987+Adelmuhammad-23@users.noreply.github.com>
2025-05-10 14:06:16 +0300

    Feat: Add update student endpoint (#31)
    * Feat: add RBAC for controllers
    
    * Feat:Add delete module and add some validation in quize
    
    * Feat: Add update student endpoint
    
    ---------
    
    Co-authored-by: Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>

---
+++ e-learning.API/Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using e_learning.Data.Helpers;
+using e_learning.Services.Abstructs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_learning.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentServices _studentServices;
+
+        public StudentsController(IStudentServices studentServices)
+        {
+            _studentServices = studentServices;
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudentAsync([FromRoute] int id, [FromForm] UpdateStudentDTO updateStudent)
+        {
+            var studentResult = await _studentServices.UpdateStudentAsync(id, updateStudent, updateStudent.Image);
+            switch (studentResult)
+            {
+                case "NotFound":
+                    return NotFound("student not found");
+                case "error in httpContext":
+                    return BadRequest("error in httpContext");
+                case "updated":
+                    return Ok("Update student is Successfully");
+                default:
+                    return BadRequest(new { message = $" {studentResult.ToString()} !!" });
+            }
+        }
+    }
+}
